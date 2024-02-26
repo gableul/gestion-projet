3fa7fe699d6e7dc19770e5872b44e506b84a2e62
gableul <gabriel.leulmi@gmail.com>
2024-02-26 16:58:50 +0100

    modif

--- app/frontend/connexion/page.js
+++ app/frontend/connexion/page.js
@@ -4,11 +4,22 @@ import React from 'react';
 import LoginForm from '@/app/component/LoginForm';
 import axios, { Axios } from 'axios';
 const LoginPage = () => {
-  async function handleLogin  (formData){
-    const data = await axios.post("http://localhost:3001/users/login", {password : formData.password,nom : formData.name})
-  }
+  const handleLogin = async (formData) => {
+    try {
+      const response = await axios.post("http://localhost:3001/users/login", {
+        password: formData.password,
+        nom: formData.name,
 
-
+      });
+  
+      // Gérer la réponse, par exemple afficher un message de succès ou rediriger l'utilisateur
+      console.log("Login successful!", response.data);
+    } catch (error) {
+      // Gérer les erreurs, par exemple afficher un message d'erreur ou des détails sur la console
+      console.error("Login failed:", error);
+    }
+  };
+  
   return (
     <div>
       <h1>Connexion</h1>
