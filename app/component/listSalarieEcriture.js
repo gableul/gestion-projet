91cb30035bf90723a305693668978d3e48a9b5f2
gableul <gabriel.leulmi@gmail.com>
2024-03-04 21:36:07 +0100

    button delete

--- app/component/listSalarieEcriture.js
+++ app/component/listSalarieEcriture.js
@@ -4,74 +4,75 @@ import "../styles/tableaux/global.css"
 const axios = require("axios")
 let liste_avec_id = []
 
-function ListSalarieEcriture(props){
+
+function ListSalarieEcriture(props) {
     const [selectEcriture, setSelectEcriture] = useState("");
-    const {setecriture} = props;
-    const {ecriture} = props;
-
-
+    const { setecriture, ecriture } = props;
+    const [liste_avec_id, setListeAvecId] = useState([]);
 
     const handleEcritureChange = (e) => {
         const selectedValue = e.target.value;
         if (!ecriture.includes(selectedValue)) {
-          setSelectEcriture(selectedValue);
-          setecriture([...ecriture, selectedValue]);
-          liste_avec_id.push(parseInt(e.target.value))
-          setSelectEcriture(selectedValue);
+            setSelectEcriture(selectedValue);
+            setecriture([...ecriture, selectedValue]);
+            setListeAvecId([...liste_avec_id, parseInt(e.target.value)]);
         }
-      };
+    };
 
+    const handleDelete = (id) => {
+        const updatedEcriture = ecriture.filter((item) => item !== id);
+        setecriture(updatedEcriture);
+        setListeAvecId(liste_avec_id.filter((item) => item !== id));
+    };
 
-      const Liste_salarie_avec_nom = () =>{
+    const Liste_salarie_avec_nom = () => {
         let liste = []
-        for(let i =0;i<props.liste_salarie.length;i++){
-            liste.push({id:props.liste_salarie[i]._id,nom:props.liste_salarie[i].nom,prenom:props.liste_salarie[i].Prenom});
+        for (let i = 0; i < props.liste_salarie.length; i++) {
+            liste.push({ id: props.liste_salarie[i]._id, nom: props.liste_salarie[i].nom, prenom: props.liste_salarie[i].Prenom });
         }
         return liste
-      }
-      const Liste_avec_nom = Liste_salarie_avec_nom()
-      const liste = Liste_avec_nom.filter((element) => {return liste_avec_id.includes(parseInt(element.id))});
-    
-      
-
+    }
+    const Liste_avec_nom = Liste_salarie_avec_nom()
+    const liste = Liste_avec_nom.filter((element) => liste_avec_id.includes(parseInt(element.id)));
 
     return (
         <>
-        Ecriture
-                    <select
-                        value={selectEcriture}
-                        onChange={handleEcritureChange}
-                    >
-                      <option> Selectionner</option>
-                         {props.liste_salarie && props.liste_salarie.map((item)=>(
-                <option value={item._id}>{item.nom}  {item.Prenom}</option>
-            ))}
-                    </select>
-                    <br></br>
-                    <div >
-                    {ecriture.length > 0 && (
-                            <table>
-                                <thead>
-                                    <tr>
-                                        <th>Nom</th>
-                                        <th>Prénom</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {selectEcriture && liste.map(selected => (
-                                        <tr key={selected.id}>
-                                            <td>{selected.nom}</td>
-                                            <td>{selected.prenom}</td>
-                                        </tr>
-                                    ))}
-                                </tbody>
-                            </table>
-                    )}
-                    </div>
-   
-            
-
-                    </>
+            Ecriture
+            <select
+                value={selectEcriture}
+                onChange={handleEcritureChange}
+            >
+                <option> Selectionner</option>
+                {props.liste_salarie && props.liste_salarie.map((item) => (
+                    <option value={item._id}>{item.nom} {item.Prenom}</option>
+                ))}
+            </select>
+            <br />
+            <div>
+                {ecriture.length > 0 && (
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Nom</th>
+                                <th>Prénom</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {selectEcriture && liste.map(selected => (
+                                <tr key={selected.id}>
+                                    <td>{selected.nom}</td>
+                                    <td>{selected.prenom}</td>
+                                    <td>
+                                        <button class="delete-button" onClick={() => handleDelete(selected.id)}>X</button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                )}
+            </div>
+        </>
     );
 };
 
